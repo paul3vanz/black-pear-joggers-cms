554fd61c9ea0f0a85030e369ad84dcfc0e46b7b0
Paul Evans <paul3vanz@gmail.com>
2021-04-01 22:55:27 +0100

    Container max width

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,5 +1,5 @@
 export const Container = ({ children }) => (
     <div className="container mx-auto">
-        <div className="mx-4">{children}</div>
+        <div className="max-w-3xl mx-auto px-4">{children}</div>
     </div>
 );
