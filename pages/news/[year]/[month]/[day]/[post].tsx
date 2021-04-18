be9aad6fac273a3eef9a18fe17025db267bb8833
Paul Evans <paul3vanz@gmail.com>
2021-04-18 06:41:58 +0100

    Moved migrated news posts

--- pages/news/[year]/[month]/[day]/[post].tsx
+++ pages/news/[year]/[month]/[day]/[post].tsx
@@ -1,20 +1,25 @@
 import { getAllBlogPosts, getBlogPostBySlug } from '../../../../../core/api';
+import matter, { GrayMatterFile } from 'gray-matter';
 
 import { Card } from '../../../../../components/Card';
 import { Cards } from '../../../../../components/Cards';
 import { Container } from '../../../../../components/Container';
 import { Copy } from '../../../../../components/Copy';
+import { Entry } from 'contentful';
 import Frontmatter from '../../../../../components/Frontmatter';
 import Head from 'next/head';
+import { IBlogPostFields } from '../../../../../@types/generated/contentful';
 import { InferGetStaticPropsType } from 'next';
 import { Layout } from '../../../../../components/Layout';
 import { Paragraph } from '../../../../../components/Paragraph';
+import ReactMarkdown from 'react-markdown';
 import { Stack } from '../../../../../components/Stack';
 import { blogPostUrl } from '../../../../../core/helpers';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import fs from 'fs';
 import moment from 'moment-mini';
 
-export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Post(props: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <div>
             <Head>
@@ -28,68 +33,123 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
                     title={props.blogPost.fields.title}
                     description={props.blogPost.fields.description}
                     publishDate={props.blogPost.fields.publishDate}
-                    heroImageUrl={props.blogPost.fields.heroImage.fields.file.url}
+                    heroImageUrl={props.blogPost.fields.heroImage?.fields.file.url}
                     author={{
-                        name: props.blogPost.fields.author.fields.name,
-                        avatarUrl: props.blogPost.fields.author.fields.image.fields.file.url,
+                        name: props.blogPost.fields.author?.fields.name,
+                        avatarUrl: props.blogPost.fields.author?.fields?.image?.fields.file.url,
                     }}
                 />
 
                 <Stack>
                     <Container>
                         <Copy>
-                            <div className="post">{documentToReactComponents(props.blogPost.fields.body)}</div>
+                            <div className="post">
+                                {typeof props.blogPost.fields.body === 'string' ? (
+                                    <ReactMarkdown children={props.blogPost.fields.body} />
+                                ) : (
+                                    documentToReactComponents(props.blogPost.fields.body)
+                                )}
+                            </div>
                         </Copy>
                     </Container>
                 </Stack>
 
-                <Stack backgroundColour="dark">
-                    <Container>
-                        <h2>Other news</h2>
+                {props.relatedBlogPosts.length && (
+                    <Stack backgroundColour="dark">
+                        <Container>
+                            <h2>Other news</h2>
 
-                        <Cards>
-                            {props.relatedBlogPosts?.map((post) => (
-                                <Card
-                                    key={post.sys.id}
-                                    headline={post.fields.title}
-                                    imageUrl={post.fields.heroImage?.fields.file.url}
-                                    content={<Paragraph content={post.fields.description} />}
-                                    link={blogPostUrl(post)}></Card>
-                            ))}
-                        </Cards>
-                    </Container>
-                </Stack>
+                            <Cards>
+                                {props.relatedBlogPosts?.map((post) => (
+                                    <Card
+                                        key={post.sys.id}
+                                        headline={post.fields.title}
+                                        imageUrl={post.fields.heroImage?.fields.file.url}
+                                        content={<Paragraph content={post.fields.description} />}
+                                        link={blogPostUrl(post)}></Card>
+                                ))}
+                            </Cards>
+                        </Container>
+                    </Stack>
+                )}
             </Layout>
         </div>
     );
 }
 
 export async function getStaticPaths() {
-    const blogPosts = await getAllBlogPosts(false);
-
     return {
-        paths: blogPosts.map((blogPost) => {
-            const publishDate = moment(blogPost.fields.publishDate);
-
-            return {
-                params: {
-                    post: blogPost.fields.slug,
-                    year: publishDate.format('YYYY'),
-                    month: publishDate.format('MM'),
-                    day: publishDate.format('DD'),
-                    publishDate: blogPost.fields.publishDate,
-                },
-            };
-        }),
+        paths: [...(await getCmsBlogPosts()), ...(await getMarkdownBlogPosts())],
         fallback: false,
     };
 }
 
 export async function getStaticProps({ params }) {
-    const blogPost = await getBlogPostBySlug(params.post, false);
-    const relatedBlogPosts = await getAllBlogPosts(false);
+    let blogPost;
+    let relatedBlogPosts = [];
+
+    const markdownPath = `./pages/news/${Object.values(params).join('/')}.md`;
+
+    if (fs.existsSync(markdownPath)) {
+        const markdown = await import(`../../../${Object.values(params).join('/')}.md`);
+        const frontmatter: GrayMatterFile<any> = matter(markdown.default);
+
+        blogPost = {
+            blogPost: {
+                fields: {
+                    slug: params.post,
+                    title: frontmatter.data.title,
+                    publishDate: frontmatter.data.date,
+                    body: frontmatter.content,
+                    author: { fields: { name: 'Black Pear Joggers', image: null } },
+                },
+            },
+        };
+    } else {
+        blogPost = await getBlogPostBySlug(params.post, false);
+        relatedBlogPosts = await getAllBlogPosts(false);
+    }
 
     return {
         props: { ...blogPost, relatedBlogPosts: relatedBlogPosts.slice(0, 3) },
     };
+}
+
+async function getCmsBlogPosts() {
+    const cmsBlogPosts: Entry<IBlogPostFields>[] = await getAllBlogPosts(false);
+
+    return cmsBlogPosts.map((blogPost) => {
+        const publishDate = moment(blogPost.fields.publishDate);
+
+        return {
+            params: {
+                post: blogPost.fields.slug,
+                year: publishDate.format('YYYY'),
+                month: publishDate.format('MM'),
+                day: publishDate.format('DD'),
+            },
+        };
+    });
+}
+
+interface StaticPathParams {
+    post: string;
+    year: string;
+    month: string;
+    day: string;
+}
+
+async function getMarkdownBlogPosts() {
+    const markdownBlogPosts = [
+        {
+            params: {
+                post: 'groups-now-available-to-book',
+                year: '2021',
+                month: '03',
+                day: '19',
+            },
+        },
+    ];
+
+    return markdownBlogPosts;
 }
